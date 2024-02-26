234c92d3d591cf7c804307aaac892915e71955fa
CatChow0 <thetrueshibadoggo@gmail.com>
2024-02-26 17:37:23 +0100

    fix

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -4,69 +4,24 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public bool isOnGround = false;
-    private CharacterController player_controller;
-    public float speed = 10.0f;
-
-    public float smoothTurn = 0.1f;
-
-    private Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float angularSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        player_controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float move_horizontal = Input.GetAxisRaw("Horizontal");
-        float move_vertical = Input.GetAxisRaw("Vertical");
-
-        moveDirection.x = move_horizontal;
-        moveDirection.z = move_vertical;
-        moveDirection.Normalize();
-
-        if (moveDirection.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurn, 0.1f);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 move = transform.forward * speed * Time.deltaTime;
-            player_controller.Move(move);
-        }
-        
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision == null)
-        {
-            return;
-        }
-
-        // Check if the player collided with the ground and not another object
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-        }
-
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        // Check if the player is no longer on the ground
-        if (collision == null)
-        {
-            return;
-        }
-
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = false;
-        }
+        float hor = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+        // float mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(transform.up, angularSpeed * mouseX);
+        transform.position = transform.position + (transform.forward * moveSpeed * vert * Time.deltaTime) + (transform.right * moveSpeed * hor * Time.deltaTime);
+        Debug.DrawRay(transform.position, transform.forward * 20, Color.blue);
+        Debug.DrawRay(transform.position, Vector3.forward * 20, Color.cyan);
     }
 }
